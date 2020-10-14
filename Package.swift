d37bbf1ccda28095685755e15d0342468ec6dc5e
Cameron Mc Gorian <cameron@sbarow.com>
2020-10-14 13:05:28 +0200

    Fix build

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "RIBs",
-            dependencies: ["RxSwift", "RxRelay"],
+            dependencies: ["RxSwift", .product(name: "RxRelay", package: "RxSwift")],
             path: "ios/RIBs"
         ),
         .testTarget(
